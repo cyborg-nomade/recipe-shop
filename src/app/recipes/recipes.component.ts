32cbc9f7aa077ea87f4a81cbb039d51fe3148bfb
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-05-30 15:04:19 -0300

    added recipe.model.ts with recipe model

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+  recipes = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
