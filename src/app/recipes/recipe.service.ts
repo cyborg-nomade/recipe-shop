b2bea3f88875bb0074e529e5c728af246832b3a2
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-06-12 15:24:11 -0300

    recipes now have ingredients!

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from 'src/app/shared/ingredients.model';
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
@@ -9,14 +10,21 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Test',
-      'This is a test',
-      'http://www.trbimg.com/img-57b62a41/turbine/ct-kfc-recipe-test-20160818'
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'http://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Test2',
-      'This is a test too',
-      'http://www.trbimg.com/img-57b62a41/turbine/ct-kfc-recipe-test-20160818'
+      'Big Fat Burger',
+      'What else do you need to say?',
+      'http://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Angus Burger', 2),
+        new Ingredient('Chesse slice', 2),
+        new Ingredient('Salad', 1)
+      ]
     )
   ];
 
