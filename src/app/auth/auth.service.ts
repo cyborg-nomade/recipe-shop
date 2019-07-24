0b1f62b8f0bd277194db82785c6f3aa90dafbb09
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-07-24 15:40:33 -0300

    login error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -29,29 +29,41 @@ export class AuthService {
           returnSecureToken: true
         }
       )
-      .pipe(
-        catchError(errorRes => {
-          let errorMessage = 'An unknown error has occurred.';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email already exists';
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDadJFsjqsCEbR3sr_98jRXxWfF3kNHsSQ',
-      {
-        email,
-        password,
-        returnSecureToken: true
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDadJFsjqsCEbR3sr_98jRXxWfF3kNHsSQ',
+        {
+          email,
+          password,
+          returnSecureToken: true
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error has occurred.';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Invalid password or e-mail';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Invalid password or e-mail';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'This user has been suspended by the administrator';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
