52186a63b9d1989aadcd381d12a778029df3c74a
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-08-02 17:13:04 -0300

    bumped version + corrected minor bug

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,7 +2,8 @@ import {
   Component,
   OnInit,
   ComponentFactoryResolver,
-  ViewChild
+  ViewChild,
+  OnDestroy
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
