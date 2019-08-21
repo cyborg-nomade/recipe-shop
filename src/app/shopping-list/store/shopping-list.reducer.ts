07042e042ee7133ba4eae88642261d66b98228ee
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-08-21 16:32:02 -0300

    included reducer in the app module

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,18 +1,21 @@
-import { Ingredient } from '../../shared/ingredients.model';
 import { Action } from '@ngrx/store';
 
-import { ADD_INGREDIENT } from './shopping-list.actions';
+import { Ingredient } from '../../shared/ingredients.model';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
   ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)]
 };
 
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.AddIngredient
+) {
   switch (action.type) {
-    case ADD_INGREDIENT:
+    case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients, action]
+        ingredients: [...state.ingredients, action.payload]
       };
     default:
       break;
