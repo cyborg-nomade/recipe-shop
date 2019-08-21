8432fb4610a45f650ffc1dba0c41d5c8c1b32d6c
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-08-21 16:15:29 -0300

    a little logic to the reducer (add ingredient)

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,7 +1,18 @@
 import { Ingredient } from '../shared/ingredients.model';
+import { Action } from '@ngrx/store';
 
 const initialState = {
   ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)]
 };
 
-export function shoppingListReducer(state = initialState, action) {}
+export function shoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action]
+      };
+    default:
+      break;
+  }
+}
