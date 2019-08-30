73690dec780de3db8d64fdaaa42770d712cb8999
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-08-30 16:36:33 -0300

    login fully workign through ngrx

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,8 +1,9 @@
-import { Observable, of } from 'rxjs';
-import { switchMap, catchError, map } from 'rxjs/operators';
+import { /* Observable, */ of } from 'rxjs';
+import { switchMap, catchError, map, tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Action } from '@ngrx/store';
+import { Router } from '@angular/router';
+/* import { Action } from '@ngrx/store'; */
 import { Actions, Effect, ofType } from '@ngrx/effects';
 
 import * as AuthActions from './auth.actions';
@@ -21,7 +22,7 @@ export interface AuthResponseData {
 @Injectable()
 export class AuthEffects {
   @Effect()
-  authLogin$ /*: Observable<Action> TODO: check how to get this type to work */ = this.actions$.pipe(
+  authLogin$ /*: Observable<Action> TODO: check how to get this type to work */ = this.action$.pipe(
     ofType(AuthActions.LOGIN_START),
     switchMap((authData: AuthActions.LoginStart) => {
       return this.http
@@ -30,7 +31,7 @@ export class AuthEffects {
             environment.firebaseAPIKey,
           {
             email: authData.payload.email,
-            password: authData.payload.email,
+            password: authData.payload.password,
             returnSecureToken: true
           }
         )
@@ -39,22 +40,50 @@ export class AuthEffects {
             const expirationDate = new Date(
               new Date().getTime() + +resData.expiresIn * 1000
             );
-            return of(
-              new AuthActions.Login({
-                email: resData.email,
-                userId: resData.localId,
-                token: resData.idToken,
-                expirationDate
-              })
-            );
+            return new AuthActions.Login({
+              email: resData.email,
+              userId: resData.localId,
+              token: resData.idToken,
+              expirationDate
+            });
           }),
-          catchError(error => {
-            // ...
-            return of();
+          catchError(errorRes => {
+            let errorMessage = 'An unknown error has occurred.';
+            if (!errorRes.error || !errorRes.error.error) {
+              return of(new AuthActions.LoginFail(errorMessage));
+            }
+            switch (errorRes.error.error.message) {
+              case 'EMAIL_EXISTS':
+                errorMessage = 'This email already exists';
+                break;
+              case 'EMAIL_NOT_FOUND':
+                errorMessage = 'Invalid password or e-mail';
+                break;
+              case 'INVALID_PASSWORD':
+                errorMessage = 'Invalid password or e-mail';
+                break;
+              case 'USER_DISABLED':
+                errorMessage =
+                  'This user has been suspended by the administrator';
+                break;
+            }
+            return of(new AuthActions.LoginFail(errorMessage));
           })
         );
     })
   );
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  @Effect({ dispatch: false })
+  authSuccess$ = this.action$.pipe(
+    ofType(AuthActions.LOGIN),
+    tap(() => {
+      this.router.navigate(['/recipes']);
+    })
+  );
+
+  constructor(
+    private action$: Actions,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 }
