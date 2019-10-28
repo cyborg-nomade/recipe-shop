5dcfb38e7ecafbebad849d008753888ae57201c3
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2019-10-28 17:23:57 -0300

    keyframes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,7 +4,8 @@ import {
   state,
   style,
   transition,
-  animate
+  animate,
+  keyframes
 } from '@angular/animations';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
@@ -91,6 +92,57 @@ import * as fromApp from '../store/app.reducer';
           style({
             transform: 'translateX(100px)',
             opacity: 0
+          })
+        )
+      ])
+    ]),
+    trigger('list2', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)'
+        })
+      ),
+      transition('void => *', [
+        animate(
+          1000,
+          keyframes([
+            style({
+              transform: 'translateX(-100px)',
+              opacity: 0,
+              offset: 0
+            }),
+            style({
+              transform: 'translateX(-50px)',
+              opacity: 0.5,
+              offset: 0.3
+            }),
+            style({
+              transform: 'translateX(-20px)',
+              opacity: 1,
+              offset: 0.8
+            }),
+            style({
+              transform: 'translateX(0px)',
+              opacity: 1,
+              offset: 1
+            })
+          ])
+        )
+      ]),
+      transition('* => void', [
+        animate(
+          300,
+          style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          })
+        ),
+        animate(
+          300,
+          style({
+            color: 'red'
           })
         )
       ])
